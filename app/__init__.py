6620dcad99dcd68d2699c678c3152670e954fa05
alex-fung <alexcfung@gmail.com>
2015-02-28 22:02:42 -0800

    Added login feature

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+import jinja2
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config.from_object('app.config')
+app.jinja_loader = jinja2.FileSystemLoader('app/templates')
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login_controller.login'
---
+++ app/config.py
@@ -0,0 +1,3 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+SQLALCHEMY_DATABASE_URI = 'postgresql://trailUser:trailPassword@traildbinstance.c9hc8vdkzflp.us-west-2.rds.amazonaws.com:5432/trailDb'
