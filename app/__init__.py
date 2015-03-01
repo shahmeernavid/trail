b41705007b7819f41abaabddfc6da1b21ec46544
alex-fung <alexcfung@gmail.com>
2015-02-28 22:55:43 -0800

    Fixed login

--- app/__init__.py
+++ app/__init__.py
@@ -9,4 +9,3 @@ app.jinja_loader = jinja2.FileSystemLoader('app/templates')
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login_controller.login'
--- app/controllers/login_controller.py
+++ app/controllers/login_controller.py
@@ -1,42 +1,78 @@
 from app import db, login_manager, app
 from app.models.user import User
 from flask_wtf import Form
-from flask import render_template, flash, redirect, request, url_for
+from flask import render_template, flash, redirect, request, url_for, session
 from wtforms import validators, TextField, PasswordField
 from flask.ext.login import login_user, logout_user, login_required
 import hashlib
+from flask_oauth import OAuth
+
+oauth = OAuth()
+
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key='860566537333704',
+    consumer_secret='ff8591f24f8712467c03c57ad52f1c00',
+    request_token_params={'scope': 'email'}
+)
+
+@facebook.tokengetter
+def get_facebook_token(token='user'):
+	return session.get('facebook_token')
+
+@app.route('/oauth-authorized')
+@facebook.authorized_handler
+def oauth_authorized(resp):
+    next_url = request.args.get('next') or url_for('index')
+    if resp is None:
+        flash(u'You denied the request to sign in.')
+        return redirect(next_url)
+
+    session['facebook_token'] = (
+        resp['oauth_token'],
+        resp['oauth_token_secret']
+    )
+    session['facebook_user'] = resp['screen_name']
+
+    flash('You were signed in as %s' % resp['screen_name'])
+    return redirect(next_url)
+
+login_manager.login_view = 'login'
 
 # Define login and registration forms (for flask-login)
 class LoginForm(Form):
-  username = TextField(validators=[validators.required()])
-  password = PasswordField(validators=[validators.required()])
+	username = TextField(validators=[validators.required()])
+	password = PasswordField(validators=[validators.required()])
 
-  def validate_login(self):
-    user = self.get_user()
+	def validate_login(self):
+		user = self.get_user()
 
-    if user is None:
-      raise validators.ValidationError('Invalid user')
+	if user is None:
+		raise validators.ValidationError('Invalid user')
 
-    # we're comparing the plaintext pw with the the hash from the db
-    if not self.check_password_hash(user.password, user.salt, self.password.data):
-      raise validators.ValidationError('Invalid password')
+	# we're comparing the plaintext pw with the the hash from the db
+	if not self.check_password_hash(user.password, user.salt, self.password.data):
+  		raise validators.ValidationError('Invalid password')
 
-  def get_user(self):
-    return db.session.query(User).filter_by(username=self.username.data).first()
+	def get_user(self):
+		return db.session.query(User).filter_by(username=self.username.data).first()
 
-  def check_password_hash(self, correctPassword, salt, password):
-  	password = hashlib.sha256(salt + password).hexdigest()
-  	return correctPassword == password
+	def check_password_hash(self, correctPassword, salt, password):
+		password = hashlib.sha256(salt + password).hexdigest()
+		return correctPassword == password
 
 @app.route("/")
 @login_required
 def index():
-  return render_template("index.html")
+	return render_template("index.html")
 
 @app.route("/test")
 @login_required
 def test():
-  return render_template("test.html")
+	return render_template("test.html")
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -52,6 +88,11 @@ def login():
 		flash("Logged In Successfully")
 		return redirect(request.args.get("next") or url_for("index"))
 	return render_template("login.html", form=form)
+
+@app.route('/facebookAuthenticate')
+def facebookAuthenticate():
+    return facebook.authorize(callback=url_for('oauth_authorized',
+        next=request.args.get('next') or request.referrer or None))
 
 @app.route("/logout")
 @login_required
